7d7ed09306503dfe84c58d4554c5a540e5ce8dca
Jaremy Creechley <creechley@gmail.com>
2024-12-11 20:02:48 -0700

    changes

--- sigils/threads.nim
+++ sigils/threads.nim
@@ -29,19 +29,6 @@ proc moveToThread*[T: Agent](agent: T, thread: AgentThread): AgentProxy[T] =
   
   return AgentProxy[T](remote: newSharedPtr(unsafeIsolate(agent)))
 
-template connect*[T](
-    a: Agent,
-    signal: typed,
-    b: AgentProxy[T],
-    slot: typed,
-    acceptVoidSlot: static bool = false,
-): void =
-  ## connects `AgentProxy[T]` to remote signals
-  ## 
-  let agentSlot = `slot`(typeof(b.remote[]))
-  checkSignalTypes(a, signal, b.remote[], agentSlot, acceptVoidSlot)
-  # a.addAgentListeners(signalName(signal), b, agentSlot)
-
 template connect*[T, S](
     a: Agent,
     signal: typed,
@@ -54,3 +41,15 @@ template connect*[T, S](
   checkSignalTypes(a, signal, b.remote[], slot, acceptVoidSlot)
   # a.addAgentListeners(signalName(signal), b, slot)
 
+template connect*[T](
+    a: Agent,
+    signal: typed,
+    b: AgentProxy[T],
+    slot: typed,
+    acceptVoidSlot: static bool = false,
+): void =
+  ## connects `AgentProxy[T]` to remote signals
+  ## 
+  let agentSlot = `slot`(typeof(b.remote[]))
+  checkSignalTypes(a, signal, b.remote[], agentSlot, acceptVoidSlot)
+  # a.addAgentListeners(signalName(signal), b, agentSlot)
