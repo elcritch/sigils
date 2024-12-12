04ebd4cfa103b93ac1bb2eabfe840eb2cf1979f8
Jaremy Creechley <creechley@gmail.com>
2024-12-11 20:13:18 -0700

    changes

--- sigils.nim
+++ sigils.nim
@@ -3,3 +3,6 @@ import sigils/signals
 import sigils/slots
 
 export signals, slots
+
+when not defined(gcArc) and not defined(gcOrc) and not defined(nimdoc):
+  {.error: "Sigils requires --gc:arc or --gc:orc".}
