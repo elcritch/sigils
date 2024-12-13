4fa8df836985879e371fbaab93193f60588922e7
Jaremy Creechley <creechley@gmail.com>
2024-12-12 22:22:26 -0700

    refactor

--- tests/tslotsThreadAsync.nim
+++ tests/tslotsThreadAsync.nim
@@ -45,6 +45,7 @@ proc completed*(self: SomeAction, final: int) {.slot.} =
   # echo "Action done! final: ", final, " (th:", getThreadId(), ")"
   self.value = final
 
+
 proc value*(self: Counter): int =
   self.value
 
