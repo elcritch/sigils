fee99b173bf92e0908a8f3abde90b6ca631512bc
Jaremy Creechley <creechley@gmail.com>
2024-12-13 00:37:30 -0700

    adding async threads

--- tests/tslotsThread.nim
+++ tests/tslotsThread.nim
@@ -86,8 +86,7 @@ suite "threaded agent slots":
     var
       a = SomeAction.new()
       b = Counter.new()
-
-    # echo "thread runner!", " (th:", getThreadId(), ")"
+    echo "thread runner!", " (th:", getThreadId(), ")"
     # echo "obj a: ", a.unsafeWeakRef
     # echo "obj b: ", b.unsafeWeakRef
     let thread = newSigilThread()
