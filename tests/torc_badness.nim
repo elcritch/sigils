9d3d62664ef2e652b0c34db8d122732e795fe628
Your Name <you@example.com>
2024-12-27 23:52:22 -0700

    debugging

--- tests/torc_badness.nim
+++ tests/torc_badness.nim
@@ -8,20 +8,13 @@ import std/unittest
 type WeakRef*[T] {.acyclic.} = object
   # pt* {.cursor.}: T
   pt*: pointer
-  ## type alias descring a weak ref that *must* be cleaned
-  ## up when an object is set to be destroyed
 
-proc `=destroy`*[T](obj: WeakRef[T]) =
-  discard
 
 template `[]`*[T](r: WeakRef[T]): lent T =
   cast[T](r.pt)
 
 proc toPtr*[T](obj: WeakRef[T]): pointer =
   result = cast[pointer](obj.pt)
-
-proc hash*[T](obj: WeakRef[T]): Hash =
-  result = hash cast[pointer](obj.pt)
 
 proc toRef*[T: ref](obj: WeakRef[T]): T =
   result = cast[T](obj)
