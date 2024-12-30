8755b61c10e80493691479726cd056321ea65774
Your Name <you@example.com>
2024-12-29 18:41:54 -0700

    need to sync destroying sharedptr and locals

--- sigils/core.nim
+++ sigils/core.nim
@@ -22,18 +22,19 @@ proc callSlots*(obj: Agent | WeakRef[Agent], req: SigilRequest) {.gcsafe.} =
         # echo ""
         # echo "call listener:tgt: ", sub.tgt, " ", req.procName
         # echo "call listener:slot: ", repr sub.slot
-        # let tgtRef = sub.tgt.toRef()
-        var res: SigilResponse = sub.tgt[].callMethod(req, sub.slot)
+        withRef sub.tgt, tgtRef:
+          # let tgtRef = sub.tgt.toRef()
+          var res: SigilResponse = tgtRef.callMethod(req, sub.slot)
 
-        when defined(nimscript) or defined(useJsonSerde):
-          discard
-        else:
-          discard
-          variantMatch case res.result.buf as u
-          of SigilError:
-            raise newException(AgentSlotError, $u.code & " msg: " & u.msg)
+          when defined(nimscript) or defined(useJsonSerde):
+            discard
           else:
             discard
+            variantMatch case res.result.buf as u
+            of SigilError:
+              raise newException(AgentSlotError, $u.code & " msg: " & u.msg)
+            else:
+              discard
 
 proc emit*(call: (Agent | WeakRef[Agent], SigilRequest)) =
   let (obj, req) = call
