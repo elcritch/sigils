4bac0aee556cb9f2c38ef4da37328c139039a635
Jaremy Creechley <creechley@gmail.com>
2025-01-04 04:30:55 -0700

    stuff

--- tests/tslotsThread.nim
+++ tests/tslotsThread.nim
@@ -266,7 +266,7 @@ suite "threaded agent slots":
       
       echo "outer done"
 
-  when false:
+  when true:
     test "agent connect then moveToThread and run":
       var
         a = SomeAction.new()
