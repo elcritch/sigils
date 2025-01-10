7219a46dac7d29f44b7494d1869c6cfba71ec167
Jaremy Creechley <creechley@gmail.com>
2025-01-09 23:02:07 -0700

    cleanup

--- sigils.nim
+++ sigils.nim
@@ -1,10 +1,11 @@
+import sigils/weakrefs
 import sigils/agents
 import sigils/signals
 import sigils/slots
 import sigils/core
 import sigils/threads
 
-export agents, signals, slots, threads, core
+export weakrefs, agents, signals, slots, threads, core
 
 when not defined(gcArc) and not defined(gcOrc) and not defined(nimdoc):
   {.error: "Sigils requires --gc:arc or --gc:orc".}
