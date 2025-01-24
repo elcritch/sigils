344f8dcaa73904cf75591aa758b6ad47abb34ffb
Jaremy Creechley <creechley@gmail.com>
2025-01-24 06:48:16 -0700

    trying reactive

--- tests/treactive.nim
+++ tests/treactive.nim
@@ -14,6 +14,9 @@ proc changed[T](tp: Reactive[T], val: T) {.signal.}
 template `{}`[T](r: Reactive[T]): T =
   r.value
 
+template `set=`[T](r: Reactive[T], val: T) =
+  emit r.changed(val)
+
 suite "reactive examples":
   test "reactive":
     let
@@ -25,7 +28,7 @@ suite "reactive examples":
     x.connect(changed, y, computed)
 
     emit x.changed(2)
-    echo "Y: ", y.value
+    # echo "Y: ", y.value
     check y.value == 4
 
     # const x = signal(5)
@@ -37,19 +40,26 @@ suite "reactive examples":
 
     template reactive[T](x: T): Reactive[T] =
       Reactive[T](value: x)
-    proc computed[T](fn: proc (): T): Reactive[T] =
-      result = Reactive[T](value: fn())
+    template computed[T](args: varargs[Reactive[T]], blk: typed): Reactive[T] =
+      block:
+        let r = Reactive[T]()
+        proc comp(res: Reactive[T]): T {.slot.} =
+          res.value = block:
+              `blk`
+        for arg in args:
+          echo "ARG: ", arg.repr
+          arg.connect(changed, r, comp, acceptVoidSlot = true)
+        r
       
     let
-      x = reactive(33)
-      y = computed() do () -> int:
+      x = reactive(5)
+      y = computed[int](x):
         2 * x{}
     
-    proc computed[T](self: Reactive[T], val: T) {.slot.} =
-      self.value = val * 2
-    x.connect(changed, y, computed)
+    printConnections(x)
 
-    emit x.changed(2)
+    x.set= 2
+    echo "X: ", x.value
     echo "Y: ", y.value
     check y.value == 4
 
