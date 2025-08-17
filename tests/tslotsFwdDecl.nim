316bbd76d913e505b83d4141fd32ca9b0f8607fe
Jaremy Creechley <creechley@gmail.com>
2025-08-17 07:30:42 -0600

    Add suppoer for pre declarations 2 (#23)
    * add fwd decl support for slots
    
    * add fwd decl support for slots

--- tests/tslotsFwdDecl.nim
+++ tests/tslotsFwdDecl.nim
@@ -1,6 +1,7 @@
 import sigils/signals
 import sigils/slots
 import sigils/core
+import std/unittest
 
 type
   FwdA* = ref object of Agent
@@ -11,19 +12,20 @@ proc valueChanged*(tp: FwdA, val: int) {.signal.}
 # Forward declare slot without a body
 proc onChange*(self: FwdA, val: int) {.slot.}
 
+suite "forward-declared slots":
+  test "predeclared slot type compiles and connects":
+    var a = FwdA()
+    connect(a, valueChanged, a, onChange)
+
 # Implement the slot later
 proc onChange*(self: FwdA, val: int) {.slot.} =
   self.value = val
 
-when isMainModule:
-  import unittest
-
-  suite "forward-declared slots":
-    test "predeclared slot type compiles and connects":
-      var a = FwdA()
-      connect(a, valueChanged, a, onChange)
-      check SignalTypes.onChange(FwdA) is (int, )
-      check a.value == 0
-      emit a.valueChanged(7)
-      check a.value == 7
-
+suite "forward-declared slots":
+  test "predeclared slot type compiles and connects":
+    var a = FwdA()
+    connect(a, valueChanged, a, onChange)
+    check SignalTypes.onChange(FwdA) is (int, )
+    check a.value == 0
+    emit a.valueChanged(7)
+    check a.value == 7
