c1ce1d30549b8b9a56284f3531de9ab9e98d617f
Jaremy Creechley <creechley@gmail.com>
2025-08-17 07:44:25 -0600

    format

--- tests/tslotsFwdDecl.nim
+++ tests/tslotsFwdDecl.nim
@@ -30,42 +30,39 @@ suite "forward-declared slots":
     emit a.valueChanged(7)
     check a.value == 7
 
-import sigils
+block testFwdDecl:
+  type
+    AppBase = ref object of Agent
 
-# framework:
-type
-  Awsm* = ref object of Agent
+    # application:
+    App = ref object of AppBase
+      foo: int
 
-# application:
-type
-  App = ref object of Awsm
-    foo: int
+  proc appEvent(self: AppBase) {.signal.}
 
-proc appEvent(self: Awsm) {.signal.}
+  proc handling2(self: App) {.slot.}
 
-proc handling2(self: App) {.slot.}
+  template replace(self: App, event: typed, handler: typed) =
+    disconnect(self, event, self)
+    connect(self, event, self, handler)
 
-template replace(self: App, event: typed, handler: typed) =
-  disconnect(self, event, self)
-  connect(self, event, self, handler)
+  proc handling1(self: App) {.slot.} =
+    self.foo = 1
+    replace(self, appEvent, handling2)
 
-proc handling1(self: App) {.slot.} =
-  self.foo = 1
-  replace(self, appEvent, handling2)
+  proc handling2(self: App) {.slot.} =
+    self.foo = 2
+    replace(self, appEvent, handling1)
 
-proc handling2(self: App) {.slot.} =
-  self.foo = 2
-  replace(self, appEvent, handling1)
-
-when isMainModule:
-  
-  let a = App()
-  connect(a, appEvent, a, handling1)
-  
-  emit a.appEvent()
-  echo "event1: ",a.foo
-  emit a.appEvent()
-  echo "event2: ",a.foo
-  
-  emit a.appEvent()
-  echo "event3: ",a.foo
+  when isMainModule:
+    
+    let a = App()
+    connect(a, appEvent, a, handling1)
+    
+    emit a.appEvent()
+    echo "event1: ",a.foo
+    emit a.appEvent()
+    echo "event2: ",a.foo
+    
+    emit a.appEvent()
+    echo "event3: ",a.foo
