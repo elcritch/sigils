e86d81364e0b6117d132857c0414b02494c7f328
Jaremy Creechley <creechley@gmail.com>
2025-08-27 01:17:34 -0600

    cleanup

--- sigils/threadProxies.nim
+++ sigils/threadProxies.nim
@@ -205,6 +205,19 @@ proc moveToThread*[T: Agent, R: SigilThread](
 
   return localProxy
 
+template connect*[T, U, S](
+    proxyTy: AgentProxy[T],
+    signal: typed,
+    b: AgentProxy[U],
+    slot: Signal[S],
+    acceptVoidSlot: static bool = false,
+): void =
+  ## connects `AgentProxy[T]` to remote signals
+  ## 
+  checkSignalTypes(T(), signal, U(), slot, acceptVoidSlot)
+  let localProxy = Agent(proxyTy)
+  localProxy.addSubscription(signalName(signal), b, slot)
+
 template connect*[T, S](
     a: Agent,
     signal: typed,
@@ -239,6 +252,19 @@ template connect*[T](
   assert not localProxy.remote.isNil
   localProxy.proxyTwin[].addSubscription(AnySigilName, localProxy.remote[], localSlot)
 
+template connect*[T, S](
+    proxyTy: AgentProxy[T],
+    signal: typed,
+    b: Agent,
+    slot: Signal[S],
+    acceptVoidSlot: static bool = false,
+): void =
+  ## connects `AgentProxy[T]` to remote signals
+  ## 
+  checkSignalTypes(T(), signal, b, slot, acceptVoidSlot)
+  let localProxy = Agent(proxyTy)
+  localProxy.addSubscription(signalName(signal), b, slot)
+
 template connect*[T](
     thr: SigilThreadPtr,
     signal: typed,
@@ -254,19 +280,6 @@ template connect*[T](
   assert not localProxy.proxyTwin.isNil
   assert not localProxy.remote.isNil
   thr.agent.addSubscription(signalName(signal), localProxy.getRemote()[], agentSlot)
-
-template connect*[T, S](
-    proxyTy: AgentProxy[T],
-    signal: typed,
-    b: Agent,
-    slot: Signal[S],
-    acceptVoidSlot: static bool = false,
-): void =
-  ## connects `AgentProxy[T]` to remote signals
-  ## 
-  checkSignalTypes(T(), signal, b, slot, acceptVoidSlot)
-  let localProxy = Agent(proxyTy)
-  localProxy.addSubscription(signalName(signal), b, slot)
 
 import macros
 
