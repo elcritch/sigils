a633e626c1de8ef1fce0cde44d8d57f2ca538ccf
Jaremy Creechley <creechley@gmail.com>
2025-11-10 01:49:54 -0700

    updates

--- sigils/svariant.nim
+++ sigils/svariant.nim
@@ -8,6 +8,11 @@ type
 
   WBuffer*[T] = VBuffer
 
+  VConcrete[T] = ref object of Variant
+    val: T
+
+template getMangledName(t: typedesc): string = $t
+
 proc asPtr*[T](wt: WBuffer[T]): ptr T =
   static: assert sizeof(T) > 0
   cast[ptr T](addr(wt.buff[0]))
@@ -20,6 +25,17 @@ proc initWrapper*[T](val: sink T): WBuffer[T] =
 proc getWrapped*(v: Variant, T: typedesc): T =
   v.get(WBuffer[T]).asPtr()[]
 
+proc resetTo*[T](v: Variant, val: sink T) =
+  let sz = sizeof(val)
+  v.typeId = getTypeId(WBuffer[T])
+  when defined(variantDebugTypes):
+    v.mangledName = getMangledName(T)
+
+  cast[VConcrete[VBuffer]](v).val.buff.setLen(sz)
+  # v.get(VBuffer).buff.setLen(sz)
+  v.get(WBuffer[T]).asPtr()[] = move val
+
+
 when isMainModule:
 
   import std/unittest
@@ -31,3 +47,9 @@ when isMainModule:
     let vx = newVariant(initWrapper(x))
     echo "=> vx: ", vx.getWrapped(int)
     check x == vx.getWrapped(int)
+
+    var y: (int, int) = (3, 14)
+    echo "y: ", y
+
+    vx.resetTo(y)
+    echo "=> vy: ", vx.getWrapped((int, int))
